fff0d92714a6affaf0b8186212c5897da008d8f6
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2015-11-30 11:30:31 -0500

    Add Sandwich creation API

--- app/controllers/sandwiches_controller.rb
+++ app/controllers/sandwiches_controller.rb
@@ -3,4 +3,16 @@ class SandwichesController < ApplicationController
     sandwiches = Sandwich.all
     render json: sandwiches
   end
+
+  def create
+    my_sandwich = Sandwich.create(sandwich_params)
+    render json: my_sandwich
+  end
+
+
+  private
+
+  def sandwich_params
+    params.require(:sandwich).permit(:name, :bread_type)
+  end
 end
